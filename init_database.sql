04fd3bd7282b5ad4ee7d69005e0860b6862f56ce
Robin <40110451+niborus@users.noreply.github.com>
2020-01-18 17:57:55 +0100

    Update init_database.sql

--- init_database.sql
+++ init_database.sql
@@ -2,10 +2,10 @@ CREATE DATABASE `topgg`;
 
 CREATE TABLE `topgg`.`votes` (
     `entry_id` INT NOT NULL AUTO_INCREMENT COMMENT 'The ID for this database.' ,
-    `bot` BIGINT NOT NULL COMMENT 'ID of the bot that received a vote' ,
-    `user` BIGINT NOT NULL COMMENT 'ID of the user who voted' ,
+    `bot` BIGINT(64) NOT NULL COMMENT 'ID of the bot that received a vote' ,
+    `user` BIGINT(64) NOT NULL COMMENT 'ID of the user who voted' ,
     `type` VARCHAR(10) NULL COMMENT 'The type of the vote (should always be \"upvote\" except when using the test button it\'s \"test\")' ,
     `isWeekend` BOOLEAN NULL COMMENT 'Whether the weekend multiplier is in effect, meaning users votes count as two' ,
-    `timestamp` BIGINT NOT NULL COMMENT 'The Unix-Timestamp, when the Request was incoming.' ,
+    `timestamp` BIGINT(64) NOT NULL COMMENT 'The Unix-Timestamp, when the Request was incoming.' ,
     PRIMARY KEY (`entry_id`)
   );
