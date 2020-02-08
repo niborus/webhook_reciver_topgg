350cc031f38d523b48f55f168d066f0b84c61937
Robin <niborus.management@gmail.com>
2020-02-08 18:33:14 +0100

    new

--- input.php
+++ input.php
@@ -16,7 +16,7 @@ $table_name = "votes";
 function kill($code, $body)
 {
         http_response_code($code);
-        die("{'error': " . $body . "'}");
+        die("{'error': '" . $body . "'}");
 }
 
 
@@ -34,8 +34,10 @@ $json = file_get_contents('php://input');
 //Converts JSON into a PHP object
 $data = json_decode($json);
 
+// For Database-Users
 if ($save == "db") {
 
+        // Preparing a MySQL-Statement
         $mysqli = new mysqli("localhost", $sql_username, $sql_passwd, $db_name);
         if ($mysqli->connect_errno) {
                 kill(500, "Failed to connect to MySQL: Check Username and Password");
@@ -44,11 +46,22 @@ if ($save == "db") {
         if (!($stmt = $mysqli->prepare("INSERT INTO ".$table_name."(bot, user, type, isWeekend, timestamp) VALUES (?, ?, ?, ?, ?)"))) {
                 kill(500, "Preparation failed");
         }
+        $stmt->bind_param("iisii", $bot_id, $user_id, $val_type, $val_isWeekend, $timestamp);
 
+        $stmt->execute();
+
+        //Reads recived Data.
+        $user_id = $data->user;
+        $bot_id = $data->bot;
+        $val_type = $data->type;
+        $val_isWeekend = $data->isWeekend;
+        
+        // Add Timestamp
+        $timestamp = date('U');
+
+        kill(200, "")
 
 }
-//Read user ID
-$user_id = $data->user;
 
 //Create a File named ID.json
 $filename = './topgg_json/'.$user_id.'.json';
